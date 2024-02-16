5e6c0a71defa9f03143d0b9f375a843022e433ca
youngsang <goyoungsang@gmail.com>
2024-02-16 12:25:03 +0900

    Initial commit

---
+++ 01_SELECT.sql
@@ -0,0 +1,27 @@
+/* SELECT */
+
+SELECT
+       MENU_NAME
+  FROM
+       TBL_MENU;
+       
+SELECT
+       MENU_CODE,
+	   MENU_NAME,
+       MENU_PRICE
+  FROM TBL_MENU;
+  
+SELECT
+       *
+  FROM TBL_MENU;
+  
+SELECT 6 + 3;
+SELECT 6 * 3;
+SELECT 6 % 3;
+
+SELECT NOW();
+SELECT CONCAT('홍.',' ', '길동') AS name;
+SELECT CONCAT('홍.',' ', '길동') AS 'Full name';
+
+
+  
